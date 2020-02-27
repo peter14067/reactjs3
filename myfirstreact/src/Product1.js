04070a332cd0d5332db251d1d001b48039f3473f
peter14067 <peter14067@gmail.com>
2020-02-27 23:01:55 +0800

    myfirstreact

--- myfirstreact/src/Product1.js
+++ myfirstreact/src/Product1.js
@@ -1,8 +1,17 @@
 import React from "react"
 import  './index4.css'; 
 import Logo1 from './pc1.jpg';
-import Logo2 from './pc2.png';
+import Logo2 from './pc2.jpg';
 import Logo3 from './pc3.jpg';
+import Logo4 from './pc4.jpg';
+import Logo5 from './pc5.jpg';
+import Logo6 from './pc6.jpg';
+import Logo7 from './pc7.jpg';
+import Logo8 from './pc8.jpg';
+import Logo9 from './pc9.jpg';
+import Logo10 from './pc10.jpg';
+
+
 import AlbumJson from './Album1.json'
 import Product from './Productexample';
 import {Row,Col,Jumbotron} from 'react-bootstrap';
@@ -37,7 +46,7 @@ class Product1 extends React.Component{
                 }
               </Row>
 
-              <Carousel cols={2} rows={1} gap={10} loop>
+              <Carousel cols={3} rows={1} gap={5} loop>
       <Carousel.Item>
         <img width="100%" src={Logo1} />
       </Carousel.Item>
@@ -47,6 +56,27 @@ class Product1 extends React.Component{
       <Carousel.Item>
         <img width="100%" src={Logo3}  />
       </Carousel.Item>
+
+      <Carousel.Item>
+        <img width="100%" src={Logo6}  />
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img width="100%" src={Logo7}  />
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img width="100%" src={Logo8}  />
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img width="100%" src={Logo9}  />
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img width="100%" src={Logo10}  />
+      </Carousel.Item>
+      
     
     </Carousel>
         </div>
