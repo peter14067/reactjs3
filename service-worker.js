0e2e3f29dfee76f3efcba0272990b7d967baa675
peter14067 <peter14067@gmail.com>
2020-03-24 16:36:14 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactjs3/precache-manifest.5509159891f9460fe2102330af221a36.js"
+  "/reactjs3/precache-manifest.e205d9ce17b65e0ad1016d187ff9118a.js"
 );
 
 self.addEventListener('message', (event) => {
